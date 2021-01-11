3329ee1f4502791dc887c99efc9c55f38397008a
Amit <amit16110@gmail.com>
2021-01-11 22:03:55 +0530

    Auth setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from "./Components/Home/Home";
 import Login from "./Components/Auth/Login";
 import Register from "./Components/Auth/Register";
 import Header from "./Components/Header/Header";
+import RegisterComplete from "./Components/Auth/RegisterComplete";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -13,6 +14,7 @@ const App = () => {
       <Header />
       <ToastContainer />
       <Switch>
+        <Route path="/registerComplete" component={RegisterComplete} />
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
         <Route path="/" component={Home} />
--- client/src/Components/Auth/RegisterComplete.js
+++ client/src/Components/Auth/RegisterComplete.js
@@ -1,41 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { auth } from "../../firebase";
 import { toast } from "react-toastify";
 
-const RegisterComplete = () => {
+const RegisterComplete = ({ history }) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    setEmail(window.localStorage.getItem("emailForRegistration"));
+  }, []);
 
   const handleSubmit = async (e) => {
     //   handle submit
     e.preventDefault();
-    console.log("env==> ", process.env.REACT_APP_REGISTER_REDIRECT_URL);
-    const config = {
-      url: process.env.REACT_APP_REGISTER_REDIRECT_URL,
-      handleCodeInApp: true,
-    };
-    await auth.sendSignInLinkToEmail(email, config);
-    toast.success(
-      `Email is sent to ${email}. Click the link to complete your registration.`
-    );
-    // save user email in local storage
-    window.localStorage.setItem("emailForRegistration", email);
-    // clear state
-    setEmail("");
+    // signIn with firebase  email link
   };
 
-  const registerForm = () => (
+  const completeRegisterForm = () => (
     <form onSubmit={handleSubmit}>
+      <input type="email" className="form-control" value={email} disabled />
       <input
-        type="email"
+        type="password"
         className="form-control"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
         autoFocus
       />
-
+      <br />
       <button type="submit" className="btn btn-raised">
-        Register
+        Complete Register
       </button>
     </form>
   );
@@ -45,8 +39,8 @@ const RegisterComplete = () => {
       {/* padding = 5 */}
       <div className="row">
         <div className="col-md-6 offset-md-3">
-          <h4>Register</h4>
-          {registerForm()}
+          <h4> Continue Register</h4>
+          {completeRegisterForm()}
         </div>
       </div>
     </div>
